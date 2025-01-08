6c8d3eafbaf71b46f73acfbb489e5f6934f7a591
ClydeMondero <clydereinyelmondero0324@gmail.com>
2025-01-08 22:29:44 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { FaGears } from "react-icons/fa6";
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <FaGears className="text-6xl" />
+
+      <h1 className="text-3xl font-bold">In Development</h1>
+    </div>
+  );
+};
+
+export default App;
