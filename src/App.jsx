786011b58c6a9fae823153c8afc674023e11bbc3
ClydeMondero <clydereinyelmondero0324@gmail.com>
2025-02-11 15:25:50 +0800

    feat: added formik, yup, and setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { FaGears } from "react-icons/fa6";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <FaGears className="text-6xl" />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={
+                    <div className="flex flex-col items-center justify-center h-screen gap-4">
+                        <FaGears className="text-6xl" />
 
-      <h1 className="text-3xl font-bold">In Development</h1>
-    </div>
-  );
+                        <h1 className="text-3xl font-bold">In Development</h1>
+                    </div>
+                } />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
